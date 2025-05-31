9e3a4a044c76151ce6689fb6e3daa2598bc8aa65
Saifudheenpv <mesaifudheenpv@gmail.com>
2025-05-31 12:56:08 +0530

    style: fix Flask app initialization indentation

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-app = Flask(__name__,
+app = Flask(
+    __name__,
     static_url_path='',
     static_folder='static',
     template_folder='templates'
